cf9b6a9f91625f95b9aaf61b1b6bf36753076028
Michael Greenhalgh <mgreenhalgh93@outlook.com>
2020-07-14 23:36:43 +0100

    Added second failing test ensuring the CoinService logic is still in place.

--- VendingKata.Tests/VendingMachineTests.cs
+++ VendingKata.Tests/VendingMachineTests.cs
@@ -6,7 +6,7 @@ namespace VendingKata.Tests
     public class VendingMachineTests
     {
         [Fact]
-        public void InsertQuarter_ReturnsCorrectValue()
+        public void InsertQuarter_ReturnsCorrectValue_25()
         {
             //Arrange
             var coinService = new CoinService();
@@ -20,5 +20,20 @@ namespace VendingKata.Tests
             Assert.Equal(25, result);
         }
 
+
+        [Fact]
+        public void InsertDime_ReturnsCorrectValue_10()
+        {
+            //Arrange
+            var coinService = new CoinService();
+
+            //Act
+            coinService.Insert(Coin.Dime, 1);
+
+            var result = coinService.Total;
+
+            //Assert
+            Assert.Equal(10, result);
+        }
     }
 }
